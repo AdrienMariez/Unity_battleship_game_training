1167eaed4dbb21cbbc969731b79a2ad5ed83359d
AdrienMariez <33837157+AdrienMariez@users.noreply.github.com>
2019-12-10 17:49:00 +0100

    Finally resolved the landing gear problem
    And simplified the animator at the same time !

--- Assets/Scripts/Aircraft/AircraftLandingGear.cs
+++ Assets/Scripts/Aircraft/AircraftLandingGear.cs
@@ -13,8 +13,7 @@ using UnityEngine;
         // this script detects the raise/lower condition and sets a parameter on
         // the animator to actually play the animation to raise or lower the gear.
 
-        public float raiseAtAltitude = 40;
-        public float lowerAtAltitude = 40;
+        public float LandingZoneDistance = 200;
 
         private GearState m_State = GearState.Lowered;
         private Animator m_Animator;
@@ -30,56 +29,28 @@ using UnityEngine;
         }
 
         private void Update() {
-            // bool x = this.GetComponent<AircraftUserControl4Axis>().m_Active;
+            float speed = Mathf.Abs (m_Rigidbody.velocity.x) + Mathf.Abs (m_Rigidbody.velocity.y) + Mathf.Abs (m_Rigidbody.velocity.z);
 
-            // float speed = Mathf.Abs (m_Rigidbody.velocity.x) + Mathf.Abs (m_Rigidbody.velocity.y) + Mathf.Abs (m_Rigidbody.velocity.z);
-
-            // if (speed < 70) {
-            //     //Debug.Log ("speed < 70");
-            //     landingZones = GameObject.FindGameObjectsWithTag("LandingZone");
-            //     bool landingZoneInProximity = false;
-            //     foreach (var zone in landingZones) {
-            //         if ( (m_Rigidbody.transform.position - zone.transform.position).magnitude < 200) {
-            //             landingZoneInProximity = true;
-            //         }
-            //     }
-            //     if (landingZoneInProximity){
-            //         Debug.Log ("landingZoneInProximity = "+landingZoneInProximity);
-            //         if (m_State == GearState.Raised){
-            //             m_State = GearState.Lowered;
-            //         }
-            //     } else {
-            //         m_State = GearState.Raised;
-            //     }
-            // } else if (m_State == GearState.Lowered) {
-            //     m_State = GearState.Raised;
-            // }
-
-            if (Input.GetButton ("FocusCamera") && m_State == GearState.Raised){
-                m_State = GearState.Lowered;
-            }
-            if (!Input.GetButton ("FocusCamera") && m_State == GearState.Lowered) {
+            if (speed < 70) {
+                //Debug.Log ("speed < 70");
+                landingZones = GameObject.FindGameObjectsWithTag("LandingZone");
+                bool landingZoneInProximity = false;
+                foreach (var zone in landingZones) {
+                    if ( (m_Rigidbody.transform.position - zone.transform.position).magnitude < LandingZoneDistance) {
+                        landingZoneInProximity = true;
+                    }
+                }
+                if (landingZoneInProximity){
+                    Debug.Log ("landingZoneInProximity = "+landingZoneInProximity);
+                    if (m_State == GearState.Raised){
+                        m_State = GearState.Lowered;
+                    }
+                } else {
+                    m_State = GearState.Raised;
+                }
+            } else if (m_State == GearState.Lowered) {
                 m_State = GearState.Raised;
             }
-
-            // Debug.Log ("m_State = "+m_State);
-
-            // Debug.Log ("m_Plane.Altitude = "+m_Rigidbody.transform.position.y);
-            // Debug.Log ("speed = "+speed);
-
-
-            // if (m_State == GearState.Lowered && m_Plane.Altitude > raiseAtAltitude && m_Rigidbody.velocity.y > 0) {
-            //     m_State = GearState.Raised;
-            //     if (!m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Raised"))
-            //         m_Animator.Play("Raised", 0, 0);
-            // }
-
-            // if (m_State == GearState.Raised && m_Plane.Altitude < lowerAtAltitude && m_Rigidbody.velocity.y < 0)
-            // {
-            //     m_State = GearState.Lowered;
-            //     if (!m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Lowered"))
-            //         m_Animator.Play("Lowered", 0, 0);
-            // }
 
             Debug.Log ("m_State = "+m_State);
 
