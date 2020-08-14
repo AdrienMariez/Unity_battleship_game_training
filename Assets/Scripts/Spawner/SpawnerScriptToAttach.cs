db3fcc2ef485650a83ef7a4e48e6a44fa3998824
AdrienMariez <33837157+AdrienMariez@users.noreply.github.com>
2020-08-14 03:39:53 +0200

    Spawner allows for randomized spawn pos

--- Assets/Scripts/Spawner/SpawnerScriptToAttach.cs
+++ Assets/Scripts/Spawner/SpawnerScriptToAttach.cs
@@ -45,11 +45,34 @@ public class SpawnerScriptToAttach : MonoBehaviour {
         // }
     }
     protected void SpawnUnit () {
-        GameObject spawnedUnitInstance =
-                Instantiate (WorldUnitsManager.m_WorldShipsUnits[0].m_UnitPrefab, m_ShipSpawnPosition.position, m_ShipSpawnPosition.rotation);
-        // WorldUnitsManager.m_WorldShipsUnits
-        // foreach (var category in m_ShipsCategories) {
-            
+        Vector3 spawnPosition = m_ShipSpawnPosition.position;
+
+        bool trySpawn = false;
+        for (int i = 0; i <= 30; i++) { // Try 30 times to spawn the unit (if it can't with 30 tries, it is deduced there is no place !)
+            spawnPosition = m_ShipSpawnPosition.position;
+            spawnPosition.x = m_ShipSpawnPosition.position.x + Random.Range(-500, 500);
+            spawnPosition.z = m_ShipSpawnPosition.position.z + Random.Range(-500, 500);
+            Collider[] hitColliders = Physics.OverlapSphere(spawnPosition, 100f);
+            if (hitColliders.Length == 0) {
+                trySpawn = true; //Spawn location correct !
+                break;
+            }
+        }
+
+        if (trySpawn) {
+            GameObject spawnedUnitInstance =
+                Instantiate (WorldUnitsManager.m_WorldShipsUnits[0].m_UnitPrefab, spawnPosition, m_ShipSpawnPosition.rotation);
+        } else {
+            Debug.Log("No spawn location available !");
+        }
+
+
+        // if (hitColliders.Length > 0) {
+        //     Debug.Log("Unit in the way !");
+        // } else {
+        //     Debug.Log("Spawn !");
+        //     GameObject spawnedUnitInstance =
+        //         Instantiate (WorldUnitsManager.m_WorldShipsUnits[0].m_UnitPrefab, spawnPosition, m_ShipSpawnPosition.rotation);
         // }
     }
 
